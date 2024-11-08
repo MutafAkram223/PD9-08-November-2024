9ad403f94d3d06c5bc5cff8fa6ee073019df1126
MutafAkram223 <2024cs223@student.uet.edu.pk>
2024-11-08 16:18:22 +0500

    Task7.cpp

--- Task7.cpp
+++ Task7.cpp
@@ -1,25 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int check(string s1, string s2);
-main()
-{
+
+int main() {
     string s1, s2;
     cout << "Enter the first string: ";
     cin >> s1;
     cout << "Enter the second string: ";
     cin >> s2;
     int result = check(s1, s2);
-    cout << "Number of matching letters are: " << result;
+    cout << "No of matching letters are: " << result << endl;
 }
-int check(string s1, string s2)
-{
-    int count;
-    for(int i=0; s1[i]!='\0'; i++)
-    {
-        for(int j=0; s2[j]!=0; j++)
-        {
-            if (s1[i] == s2[j])
-            count++;
+
+int check(string s1, string s2) {
+    int count = 0;
+    for (int i = 0; s1[i] != '\0'; i++) {
+        for (int j = 0; s2[j] != '\0'; j++) {
+            if (s1[i] == s2[j]) {
+                count++;
+                s2[j] = '*'; 
+                break;
+            }
         }
     }
     return count;
